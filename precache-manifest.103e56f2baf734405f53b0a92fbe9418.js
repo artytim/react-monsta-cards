1142802a33b42b29f42e5f1119b6f0de92e643a2
timshum <timshum@artytect.com>
2021-07-16 20:08:34 +0800

    Updates

---
+++ precache-manifest.103e56f2baf734405f53b0a92fbe9418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1803b7d1b6dfc2cd01a379e0c7d89b52",
+    "url": "/react-monsta-cards/index.html"
+  },
+  {
+    "revision": "5517f4f63c824bde9022",
+    "url": "/react-monsta-cards/static/css/main.11319235.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/react-monsta-cards/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-monsta-cards/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5517f4f63c824bde9022",
+    "url": "/react-monsta-cards/static/js/main.1d53bc91.chunk.js"
+  },
+  {
+    "revision": "c97981ea4804ddc30850",
+    "url": "/react-monsta-cards/static/js/runtime-main.c43fa166.js"
+  }
+]);
